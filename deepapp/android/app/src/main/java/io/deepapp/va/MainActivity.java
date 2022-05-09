759a6f25b3c571ab0127055464ad6c0205b68e0e
aurelcamp <29278185+aurelcamp@users.noreply.github.com>
2022-05-09 19:14:07 +0200

    u

---
+++ deepapp/android/app/src/main/java/io/deepapp/va/MainActivity.java
@@ -0,0 +1,5 @@
+package io.deepapp.va;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ deepapp/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor(private router: Router, private zone: NgZone) {
+    this.initializeApp();
+  }
+ 
+  initializeApp() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      this.zone.run(() => {
+        const domain = 'deepapp-aurelcamp.surge.sh';
+ 
+        const pathArray = event.url.split(domain);
+        // The pathArray is now like ['https://deepapp-aurelcamp.surge.sh', '/details/42']
+ 
+        // Get the last element with pop()
+        const appPath = pathArray.pop();
+        if (appPath) {
+          // this.router.navigateByUrl(appPath);
+          alert(appPath);
+        }
+      });
+    });
+  }
+}
